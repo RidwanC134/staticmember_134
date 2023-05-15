088b597a7a89b3dbcf22aa8bb5ba7d3163e3d371
RidwanC134 <ridwanhh10@gmail.com>
2023-05-15 20:00:54 +0700

    fix problem typo setNim

--- mbrStaticFunc_134/mbrStaticFunc_134.cpp
+++ mbrStaticFunc_134/mbrStaticFunc_134.cpp
@@ -33,7 +33,7 @@ void mahasiswa::printAll() {
 int main() {
 	mahasiswa mhs1("Sri Dadi");
 	mahasiswa mhs2("Budi Jatmiko");
-	mahasiswa::setNim(9); //mengakses nim melalui static member function "setnim"
+	mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
 	mahasiswa mhs3("Andi Janu");
 	mahasiswa mhs4("Joko Wahono");
 
