958af71972f6bb979851de34b655c05afcbd499c
RidwanC134 <ridwanhh10@gmail.com>
2023-05-15 19:18:57 +0700

    Fix problem typo

--- destruktor_134/destruktor_134.cpp
+++ destruktor_134/destruktor_134.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 class angka {
 private:
-	int* arr;
+	int *arr;
 	int panjang;
 public:
-	angka(int); // Constructor
-	~angka(); // Destructor
+	angka(int); //Constructor
+	~angka(); //Destructor
 	void cetakData();
 	void isiData();
 };
+//Definisi member Function
+angka::angka(int i){ //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
