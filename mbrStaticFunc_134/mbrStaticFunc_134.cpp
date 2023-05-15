92616b80e2d181e5b2ced88da10deb7aa42161b8
RidwanC134 <ridwanhh10@gmail.com>
2023-05-15 20:00:26 +0700

    Membuat akses nim melalui static member function getNim

--- mbrStaticFunc_134/mbrStaticFunc_134.cpp
+++ mbrStaticFunc_134/mbrStaticFunc_134.cpp
@@ -34,5 +34,13 @@ int main() {
 	mahasiswa mhs1("Sri Dadi");
 	mahasiswa mhs2("Budi Jatmiko");
 	mahasiswa::setNim(9); //mengakses nim melalui static member function "setnim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
 
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //mengakses nim melalui static member function "getNim"
 }
