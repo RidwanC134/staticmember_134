70780c219052a73d22156c0bed2f92d0af2fa2d0
RidwanC134 <ridwanhh10@gmail.com>
2023-05-15 19:57:41 +0700

    Membuat akses nim melalui static member function setnim

--- mbrStaticFunc_134/mbrStaticFunc_134.cpp
+++ mbrStaticFunc_134/mbrStaticFunc_134.cpp
@@ -15,5 +15,24 @@ public:
 	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/}
 	static int getNim() { return nim; /*Definisi Function*/}
 
-	
+	mahasiswa(string pnama) :nama(pnama) { setID();}
 };
+
+int mahasiswa::nim = 0; 
+
+void mahasiswa::setID(){
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengakses nim melalui static member function "setnim"
+
+}
